15a6e5e31edcb777e5b6951975ae6936b89fba46
raghavbobal1 <Shivji7&>
2020-02-13 18:51:24 -0500

    [Raghav Bobal] Created a bill file and a bill class with some variables in it.

---
+++ C0780996_W2020_MID_TERM_MAD3004/C0780996_W2020_MID_TERM_MAD3004/Bill.swift
@@ -0,0 +1,16 @@
+//
+//  Bill.swift
+//  C0780996_W2020_MID_TERM_MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-13.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+class bill
+{
+    private var billId: Int
+    private var billDate: Date?
+    private var totalBillAmount: Float
+}
