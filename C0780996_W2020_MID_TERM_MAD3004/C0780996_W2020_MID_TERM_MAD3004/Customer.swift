315b4e0b1547cc52c14623cae2366ac65a817530
raghavbobal1 <Shivji7&>
2020-02-14 17:06:08 -0500

    [Raghav Bobal] created a customer file and class with some  variables

---
+++ C0780996_W2020_MID_TERM_MAD3004/C0780996_W2020_MID_TERM_MAD3004/Customer.swift
@@ -0,0 +1,23 @@
+//
+//  Customer.swift
+//  C0780996_W2020_MID_TERM_MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-14.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+public class Customer: Bill
+{
+    private var customerId: Int
+    private var firstName: String
+    private var lastName: String
+    private var fullName: String
+    private var emailId: String
+    private var billDictionary: String
+    private var totalAmount: Float??
+    
+    
+}
+
