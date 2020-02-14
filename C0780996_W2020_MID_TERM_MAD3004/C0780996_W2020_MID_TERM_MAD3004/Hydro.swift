1092541cd4c50867b2ff82b4326aa385683ed132
raghavbobal1 <Shivji7&>
2020-02-13 20:38:52 -0500

    [Raghav Bobal] Created a hydro file with hydro class and some variables

---
+++ C0780996_W2020_MID_TERM_MAD3004/C0780996_W2020_MID_TERM_MAD3004/Hydro.swift
@@ -0,0 +1,16 @@
+//
+//  Hydro.swift
+//  C0780996_W2020_MID_TERM_MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-13.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+class Hydro: Bill
+{
+    private var agencyName: String
+    private var unitConsumed: Float
+
+}
