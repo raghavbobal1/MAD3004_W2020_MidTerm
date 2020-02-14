e8b35e1cc5970d8b67e6d02e323f4e24f785547b
raghavbobal1 <Shivji7&>
2020-02-14 18:56:42 -0500

    [Raghav Bobal] created file insurance and class with some variables

---
+++ C0780996_W2020_MID_TERM_MAD3004/C0780996_W2020_MID_TERM_MAD3004/Insurance .swift
@@ -0,0 +1,29 @@
+//
+//  Insurance .swift
+//  C0780996_W2020_MID_TERM_MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-14.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+enum TypeOfInsurance {
+    case House, Car, Business, Health
+}
+
+
+public class Insurance
+    
+{
+  private var insuranceProviderName: String
+  private var typeofInsurance: TypeOfInsurance
+  private var startDate: Date
+  private var endDate: Date
+  private var totalDays: Int
+  private var totalInsuranceAmount: Double
+    
+    
+  
+
+}
