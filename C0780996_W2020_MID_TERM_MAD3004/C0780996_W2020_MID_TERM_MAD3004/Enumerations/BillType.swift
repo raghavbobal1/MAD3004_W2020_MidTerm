1d83450f69a9a35ace59915dfa51f0cae6d1b999
raghavbobal1 <Shivji7&>
2020-02-14 21:18:45 -0500

    [Raghav Bobal] Added 3 bill types

---
+++ C0780996_W2020_MID_TERM_MAD3004/C0780996_W2020_MID_TERM_MAD3004/Enumerations/BillType.swift
@@ -0,0 +1,15 @@
+//
+//  BillType.swift
+//  C0780996_W2020_MID_TERM_MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-14.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+enum BillType
+{
+    case Hydro, Mobile, Internet
+    
+}
