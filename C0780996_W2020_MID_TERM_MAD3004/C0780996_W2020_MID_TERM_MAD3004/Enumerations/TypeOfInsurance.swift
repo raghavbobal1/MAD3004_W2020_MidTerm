290724fec953d306daf22f4b5d00ec7878640ae9
raghavbobal1 <Shivji7&>
2020-02-14 21:24:36 -0500

    [Raghav Bobal] Created typeofinsurance enum for difference insurance types.

---
+++ C0780996_W2020_MID_TERM_MAD3004/C0780996_W2020_MID_TERM_MAD3004/Enumerations/TypeOfInsurance.swift
@@ -0,0 +1,14 @@
+//
+//  TypeOfInsurance.swift
+//  C0780996_W2020_MID_TERM_MAD3004
+//
+//  Created by Raghav Bobal on 2020-02-14.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+enum TypeOfInsurance
+{
+    case Home, Car, Business, Health
+}
